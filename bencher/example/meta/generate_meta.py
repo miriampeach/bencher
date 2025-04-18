f4f0f2bd7b4adb428149bb84c80e2dc16cd1cdb9
Austin Gregg-Smith <blooop@gmail.com>
2025-04-18 22:19:16 +0000

    update params

--- bencher/example/meta/generate_meta.py
+++ bencher/example/meta/generate_meta.py
@@ -14,7 +14,7 @@ class BenchMetaGen(bch.ParametrizedSweep):
 
     # Variables for controlling the sweep
     float_vars_count = bch.IntSweep(
-        default=0, bounds=(0, 2), doc="The number of floating point variables that are swept"
+        default=0, bounds=(0, 3), doc="The number of floating point variables that are swept"
     )
     categorical_vars_count = bch.IntSweep(
         default=0, bounds=(0, 2), doc="The number of categorical variables that are swept"
