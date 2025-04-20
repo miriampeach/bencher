ebb698cef5ea05dc2239c394ea41b7fff5e9013a
Austin Gregg-Smith <blooop@gmail.com>
2025-04-20 12:32:06 +0000

    Add all untracked files

--- bencher/example/meta/generate_examples.py
+++ bencher/example/meta/generate_examples.py
@@ -66,6 +66,10 @@ if __name__ == "__main__":
     )
 
     convert_example_to_jupyter_notebook(
+        "/workspaces/bencher/bencher/example/inputs_1D/example_3_cat_in_2_out.py", "1D"
+    )
+
+    convert_example_to_jupyter_notebook(
         "/workspaces/bencher/bencher/example/inputs_2D/example_2_cat_in_4_out_repeats.py", "1D"
     )
 
